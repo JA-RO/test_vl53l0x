762ae2e08b44c30f7f83929297b351ad469bfd93
JA-RO <michel-gerald.kangmeni@epitech.eu>
2024-02-23 21:25:11 +0100

    update test_vl53l0x.py

--- test_vl53l0x.py
+++ test_vl53l0x.py
@@ -11,20 +11,20 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create sensor instance
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
-while True:
-    try:
-        # Read the distance
-        distance_mm = vl53.range
-        print("Distance:", distance_mm, "mm")
-        time.sleep(0.1)
-    except RuntimeError as e:
-        print("Error:", e)
-
-# mpx = Mpx(5)
-#
 # while True:
-#     scan = mpx.get_scan()
-#     print("---- New scan -----")
-#     for i in range(len(scan)):
-#         print("%d: %f" % (i + 1, scan[i]))
-#     sleep(1)
+#     try:
+#         # Read the distance
+#         distance_mm = vl53.range
+#         print("Distance:", distance_mm, "mm")
+#         time.sleep(0.1)
+#     except RuntimeError as e:
+#         print("Error:", e)
+
+mpx = Mpx(5)
+
+while True:
+    scan = mpx.get_scan()
+    print("---- New scan -----")
+    for i in range(len(scan)):
+        print("%d: %f" % (i + 1, scan[i]))
+    sleep(1)
