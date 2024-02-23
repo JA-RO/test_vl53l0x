6bafacdba4c569a254fef3266bc122dd028fc43c
JA-RO <michel-gerald.kangmeni@epitech.eu>
2024-02-23 20:44:29 +0100

    add mpx.py

---
+++ mpx.py
@@ -0,0 +1,99 @@
+from threading import Lock, Thread
+from time import sleep
+import adafruit_vl53l0x
+from adafruit_mcp230xx.mcp23017 import MCP23017
+import digitalio
+import board
+import busio
+import time
+
+# TODO:
+# - Exceptions management
+# - Attribute a placement to sensors (front, back, sides)
+
+# Default adress of Vl53L0X
+DEFAULT_ADDRESS = 0x29
+MAX_DIST = 3000
+
+
+# MDB Class
+
+# __init__(nb_sensors, sensor_address, expander_address, refresh):
+#   - nb_sensors        : Number of VL53L0X
+#   - sensor_address    : Address of the first sensors
+#   - expander_address  : Address of the Gpio Expander
+#   - refresh           : Refresh of the loop
+# Sensors wil be between sensor_address and sensor_address + nb_sensors - 1
+# Will init the gpio expanders and sensors
+# Will launch a thread for the loop
+
+# loop():
+# Loop to get scan with a certain refresh
+# Must run in a thread
+# Will use a lock to update the data of the class
+
+# get_scan:
+#   Will return a scan (a list of measurement)
+
+# __str__():
+# Return a string of the status of the MDB
+
+class Mpx:
+
+    def __init__(self, nb_sensors, sensor_address=0x29, expander_address=0x20, refresh=0.1, debug=False):
+        self.coef = 255 / (MAX_DIST / 2)
+
+        # Init I2C bus
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensors = []
+        self.scan = []
+
+        # Init MCP
+        self.mcp = MCP23017(self.i2c, address=expander_address)
+
+        self.nb_sensors = nb_sensors
+        self.refresh = refresh
+        self.debug = debug
+
+        # Init all sensors
+        self.init_sensors(sensor_address)
+        print(nb_sensors + '\n' + sensor_address + '\n' + expander_address + ":" + refresh + ":" + debug)
+
+        self.lock = Lock()
+        Thread(target=self.loop).start()
+
+    def init_sensors(self, sensors_address):
+
+        # Put all XSHUT pins to LOW
+        for i in range(0, self.nb_sensors):
+            self.mcp.get_pin(i).switch_to_output()
+            self.mcp.get_pin(i).value = False
+
+        # Init sensor one by one and change addresses
+        for i in range(0, self.nb_sensors):
+            self.mcp.get_pin(i).value = True
+            self.sensors.append(adafruit_vl53l0x.VL53L0X(self.i2c))  # , address=DEFAULT_ADDRESS))
+            self.sensors[i].set_address(sensors_address + i + 1)
+
+    def loop(self):
+        while True:
+
+            tmp = []
+
+            for i in range(0, self.nb_sensors):
+                dist = self.sensors[i].range
+                tmp.append(dist)
+            # Update scan
+            with self.lock:
+                self.scan = tmp
+
+            time.sleep(self.refresh)
+
+    def get_scan(self):
+        tmp = []
+        with self.lock:
+            tmp = self.scan
+        return tmp
+
+    def __str__(self):
+        return ""
--- test_vl53l0x.py
+++ test_vl53l0x.py
@@ -2,6 +2,8 @@ import time
 import board
 import busio
 import adafruit_vl53l0x
+from mpx import Mpx
+from time import sleep
 
 # Create I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -17,3 +19,12 @@ while True:
         time.sleep(0.1)
     except RuntimeError as e:
         print("Error:", e)
+
+# mpx = Mpx(5)
+#
+# while True:
+#     scan = mpx.get_scan()
+#     print("---- New scan -----")
+#     for i in range(len(scan)):
+#         print("%d: %f" % (i + 1, scan[i]))
+#     sleep(1)
